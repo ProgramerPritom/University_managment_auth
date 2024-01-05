abedfd35190e5c91080dd42ded338d19bb279aaa
ProgrammerPritom <badruzzamanpritom@gmail.com>
2024-01-06 00:08:39 +0600

    added new extention eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express();
-const port = 3000;
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Base Url..')
+})
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Welcome to Base Url..');
-});
-
-export default app;
+export default app
